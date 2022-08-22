a287abc7cc319b31b28ad93e82fc175b25b0dc66
elizjo <eli3jo.art@gmail.com>
2022-08-21 17:18:18 -0700

    Updates

---
+++ precache-manifest.14154972ba977d63a8e42d471e4a0eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a1565fd1703a03ef59b",
+    "url": "/react-tutorial/static/css/main.56ab79c6.chunk.css"
+  },
+  {
+    "revision": "6a1565fd1703a03ef59b",
+    "url": "/react-tutorial/static/js/main.6a97fadd.chunk.js"
+  },
+  {
+    "revision": "31ec15b5c37a2081fb97",
+    "url": "/react-tutorial/static/js/runtime~main.69d83b59.js"
+  },
+  {
+    "revision": "0b7ceb08ccb782bb5bf4",
+    "url": "/react-tutorial/static/js/2.1fee5dd3.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/react-tutorial/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "1d096dc991d01a6f1b24f7a99b525d52",
+    "url": "/react-tutorial/index.html"
+  }
+];
